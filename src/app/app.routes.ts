52228b55268180bb117798b8b5cd2f9cce424f10
richrdPere <richrdpere1234@gmail.com>
2025-04-18 09:36:55 -0500

    creacion de proyecto y de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
+];
---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  templateUrl: './dashboard-page.component.html',
+})
+export default class DashboardPageComponent {
+
+}
